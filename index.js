66afed56625c42e1519bdc2126b91f614cd6e498
Ekoramdan <76690451+Ekoramdan@users.noreply.github.com>
2020-12-29 12:34:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'EKO JNBOT'; 
+const instagram = 'https://instagram.com/eko_rmdnn';  
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/ekormdnn';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
